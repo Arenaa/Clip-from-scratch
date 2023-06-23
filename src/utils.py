3caa434bf1048bf67150c433e5c4e53493b8bea3
Mahsa <khoshnoodi.ma@gmail.com>
2023-06-23 23:34:13 +0330

    add some classess to utils module

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,26 @@
+from functools import partial
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch import einsum
-from einops import rearrange, repeat, reduce
+from einops import rearrange, reduce, repeat
 from einops.layers.torch import Rearrange, Reduce
+from torch import einsum
 
+
+def exists(val):
+    return val is not None
+
+def rotate_half(x):
+    x = rearrange(x, '... (j d) -> ... j d', j = 2)
+    x1, x2 = x.unbind(dim = -2)
+    return torch.cat((-x2, x1), dim = -1)
+
+def apply_rotary_pos_emb(freqs, t):
+    rot_dim = freqs.shape[-1]
+    t, t_pass = t[..., :rot_dim], t[..., rot_dim:]
+    t = (t * freqs.cos()) + (rotate_half(t) * freqs.sin())
+    return torch.cat((t, t_pass), dim = -1)
 
 class LayerNorm(nn.Module):
     def __init__(self, dim):
@@ -44,3 +60,81 @@ class PatchDropout(nn.Module):
 
         return x[batch_indices, patch_indices_keep]
 
+class RotaryEmbedding(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        pos = 1. / (10000 ** (torch.range(0, dim, 2).float()/dim))
+        self.register_buffer('pos', pos)
+
+    def forward(self, seq_len, device):
+        pos = self.pos
+        t = torch.erange(seq_len, device=device).type_as(pos)
+        freqs = torch.einsum('i, j -> i j', t, pos)
+        return torch.cat((freqs, freqs), dim=-1)
+
+class GEGLU(nn.Module):
+    def forward(self, x):
+        x, gate = x.chunk(2, dim=-1)
+        return x*F.gelu(x)
+
+class FeedForward(nn.Module):
+    def __init__(self, dim, mult=4, dropout=0.):
+        super().__init__()
+        inner_dim = int(dim * mult)
+
+        self.net = nn.Sequential(
+            nn.Linear(dim, inner_dim * 2, bias=False),
+            GEGLU(),
+            LayerNorm(inner_dim),
+            nn.Dropout(dropout),
+            nn.Linear(inner_dim, dim, bias=False)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+class Attention(nn.Module):
+    def __init__(self, dim, dim_head=64, heads=8, causal=False, dropout=0.):
+        super().__init__()
+        self.heads = heads
+        self.causal = causal
+        self.scale = dim_head ** -0.5
+        inner_dim = dim_head * heads
+
+        self.qkv = nn.Linear(dim, inner_dim*3, bias=False)
+        self.out = nn.Sequential(nn.Linear(inner_dim, dim, bias=False), LayerNorm(dim))
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, mask=None, rotary_pos_emb=None):
+        h, device, scale = self.heads, x.device, self.scale
+
+        q, k, v = self.qkv(x).chunk(3, dim=-1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = h), (q, k, v))
+
+        q = q * self.scale
+
+        if exists(rotary_pos_emb):
+            apply_rotary = partial(apply_rotary_pos_emb, rotary_pos_emb)
+            q, k, v = map(apply_rotary, (q, k, v))
+
+        sim = einsum('b h i d, b h j d -> b h i j', q, k)
+
+        mask_value = -torch.finfo(sim.dtype).max
+
+        if exists(mask):
+            mask = rearrange(mask, 'b j -> b 1 1 j')
+            sim = sim.masked_fill(~mask, mask_value)
+
+        if self.causal:
+            i, j = sim.shape[-2:]
+            causal_mask = torch.ones((i, j), dtype=torch.bool, device=device).triu(j - i + 1)
+            sim = sim.masked_fill(causal_mask, mask_value)
+
+        attn = sim.softmax(dim=-1, dtype=torch.float32)
+        attn = attn.type(sim.dtype)
+
+        attn = self.dropout(attn)
+
+        out = einsum('b h i j, b h j d -> b h i d', attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        return self.to_out(out)
