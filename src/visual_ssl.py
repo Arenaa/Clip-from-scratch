3e9c207ee4b20f472760425d888e53abcf3c54f5
Mahsa <khoshnoodi.ma@gmail.com>
2023-07-12 01:19:49 +0330

    complete visual ssl

--- src/visual_ssl.py
+++ src/visual_ssl.py
@@ -72,3 +72,206 @@ def set_requires_grad(model, val):
 
 def l2norm(t):
     return F.normalize(t, p = 2, dim = -1)
+
+def contrastive_loss(queries, keys, temperature=0.1):
+    b, device = queries.shape[0], queries.device
+    logits = queries @ keys.t()
+    logits = logits - logits.max(dim=-1, keepdim=True).values
+    logits /= temperature
+    return F.cross_entropy(logits, torch.arange(b, device=device))
+
+
+def nt_xent_loss(queries, keys, temperature=0.1):
+    b, device = queries.shape[0], queries.device
+
+    n = b * 2
+    projs = torch.cat((queries, keys))
+    logits = projs @ projs.t()
+
+    mask = torch.eye(n, device=device).bool()
+    logits = logits[~mask].reshape(n, n-1)
+    logits /= temperature
+
+    labels = torch.cat(((torch.arange(b, device=device) + b -1), torch.arange(b, device=device)), dim=0)
+    loss = F.cross_entropy(logits, labels, reduction='sum')
+    return loss
+
+def loss_fn(x, y):
+    x = l2norm(x)
+    y = l2norm(y)
+    return 2 - 2 * (x * y).sum(dim=-1)
+
+def MLP(dim, projection_size, hidden_size = None):
+    hidden_size = default(hidden_size, dim)
+
+    return nn.Sequential(
+        nn.Linear(dim, hidden_size),
+        nn.BatchNorm1d(hidden_size),
+        nn.ReLU(inplace=True),
+        nn.Linear(hidden_size, projection_size
+        )
+    )
+
+def SimSiamMLP(dim, projection_size, hidden_size=4000):
+    hidden_size = default(hidden_size, projection_size * 2)
+
+    return nn.Sequential(
+        nn.Linear(dim, hidden_size, bias=False),
+        nn.BatchNorm1d(hidden_size),
+        nn.ReLU(inplace=True),
+        nn.Linear(hidden_size, hidden_size, bias=False),
+        nn.BatchNorm1d(hidden_size),
+        nn.ReLU(inplace=True),
+        nn.Linear(hidden_size, projection_size, bias=False),
+        nn.BatchNorm1d(projection_size, affine=False)
+    )
+
+class NetWrapper(nn.Module):
+    def __init__(self, net, projection_size, projection_hidden_size=4096, layer=-2):
+        super().__init__()
+        self.net = net
+        self.layer = layer
+
+        self.projector = None
+        self.projection_size = projection_size
+        self.projection_hidden_size = projection_hidden_size
+
+        self.hidden = []
+        self.hook_registered = False
+
+    def find_layer(self):
+        if type(self.layer) == str:
+            modules = dict([*self.net.named_modules()])
+            return modules.get(self.layer, None)
+        elif type(self.layer) == int:
+            children = [*self.net.children()]
+            return children[self.layer]
+        return None
+
+    def _hook(self, _, input, output):
+        device = input[0].device
+        self.hidden[device] = flatten(output)
+
+    def _register_hook(self):
+        layer = self._find_layer()
+        assert layer is not None, f'hidden layer ({self.layer}) not found'
+        handle = layer.register_forward_hook(self._hook)
+        self.hook_registered = True
+
+    @singleton('projector')
+    def _get_projector(self, hidden):
+        _, dim = hidden.shape
+        projector = SimSiamMLP(dim, self.projection_size, self.projection_hidden_size)
+        return projector.to(hidden)
+
+    def get_representation(self, x):
+        if self.layer == -1:
+            return self.net(x)
+
+        if not self.hook_registered:
+            self._register_hook()
+
+        self.hidden.clear()
+        _ = self.net(x)
+        hidden = self.hidden[x.device]
+        self.hidden.clear()
+
+        assert hidden is not None, f'hidden layer {self.layer} never emitted an output'
+        return hidden
+
+    def forward(self, x, return_projection = True):
+        representation = self.get_representation(x)
+
+        if not return_projection:
+            return representation
+
+        flattened_representation = rearrange(representation, '... d -> (...) d')
+        projector = self._get_projector(flattened_representation)
+        projection = projector(flattened_representation)
+        return projection, representation
+
+class SimSiam(nn.Module):
+    def __init__(self, net, image_size, channels=3, hidden_laayer = -2,
+                 projection_size = 256, projection_hidden_size = 4096,
+                 augment_fn = None,
+                 augment_fn2 = None):
+        super().__init__()
+
+        self.net = net
+
+        # default SimCLR augmentation
+
+        self.augment1 = default(augment_fn, get_default_aug(image_size, channels))
+        self.augment2 = default(augment_fn2, self.augment1)
+
+        self.online_encoder = NetWrapper(net, projection_size, projection_hidden_size, layer=hidden_layer)
+        self.online_predictor = MLP(projection_size, projection_size, projection_hidden_size)
+
+        # get device of network and make wrapper same device
+        device = get_module_device(net)
+        self.to(device)
+
+        # send a mock image tensor to instantiate singleton parameters
+        self.forward(torch.randn(2, channels, image_size, image_size, device=device))
+
+        def forward(self, x):
+
+            image_one, image_two = self.augment1(x), self.augment2(x)
+
+            online_proj_one, _ = self.online_encoder(image_one)
+            online_proj_two, _ = self.online_encoder(image_two)
+
+            online_pred_one = self.online_predictor(online_proj_one)
+            online_pred_two = self.online_predictor(online_proj_two)
+
+            with torch.no_grad():
+                target_encoder = self.online_encoder
+                target_proj_one, _ = target_encoder(image_one)
+                target_proj_two, _ = target_encoder(image_two)
+                target_proj_one.detach_()
+                target_proj_two.detach_()
+
+            loss_one = loss_fn(online_pred_one, target_proj_two)
+            loss_two = loss_fn(online_pred_two, target_proj_one)
+
+            loss = loss_one + loss_two
+            return loss.mean()
+
+
+class SimCLR(nn.Module):
+    def __init__(
+        self,
+        net,
+        image_size,
+        channels = 3,
+        hidden_layer = -2,
+        project_hidden = True,
+        project_dim = 128,
+        augment_both = True,
+        use_nt_xent_loss = False,
+        augment_fn = None,
+        temperature = 0.1
+    ):
+        super().__init__()
+        self.net = NetWrapper(net, project_dim, layer = hidden_layer)
+        self.augment = default(augment_fn, get_default_aug(image_size, channels))
+        self.augment_both = augment_both
+        self.temperature = temperature
+
+        # get device of network and make wrapper same device
+        device = get_module_device(net)
+        self.to(device)
+
+        # send a mock image tensor to instantiate parameters
+        self.forward(torch.randn(1, channels, image_size, image_size))
+
+    def forward(self, x):
+        b, c, h, w, device = *x.shape, x.device
+        transform_fn = self.augment if self.augment_both else noop
+
+        queries, _ = self.net(transform_fn(x))
+        keys, _    = self.net(self.augment(x))
+
+        queries, keys = map(flatten, (queries, keys))
+        loss = nt_xent_loss(queries, keys, temperature = self.temperature)
+        return loss
